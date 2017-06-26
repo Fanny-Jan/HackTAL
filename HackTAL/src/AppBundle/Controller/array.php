dd9dab4bbc27e9c5fe33b1389f8bd3f92dfdddb0
Fanny-Jan <fanny.jan12@gmail.com>
2017-06-26 02:45:39 +0200

    css

---
+++ HackTAL/src/AppBundle/Controller/array.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder12
+ * Date: 26/06/17
+ * Time: 01:29
+ */
+$recap = array(
+    '0' => [
+        'name' => 'toto',
+        'tag' => 'ettet,ertert,ertert,erterter,ertert',
+        1 => 4,
+        2 => 5,
+        3 => 7,
+        4 => 2,
+        5 => 4.5,
+        6 => 5,
+        'penality' => 1,
+        'p1' => 0,
+        'p2' => 0,
+        'p3' => 0,
+        'p4' => 0,
+        'p5' => 0,
+        'p6' => 0,
+    ],
+    '1' => [
+        'name' => 'toto',
+        'tag' => 'ettet,ertert,ertert,erterter,ertert',
+        1 => 4,
+        2 => 5,
+        3 => 7,
+        4 => 2,
+        5 => 4.5,
+        6 => 5,
+        'penality' => 1,
+        'p1' => 0,
+        'p2' => 0,
+        'p3' => 0,
+        'p4' => 0,
+        'p5' => 0,
+        'p6' => 0,
+    ],
+    '2' => [
+        'name' => 'toto',
+        'tag' => 'ettet,ertert,ertert,erterter,ertert',
+        1 => 4,
+        2 => 5,
+        3 => 7,
+        4 => 2,
+        5 => 4.5,
+        6 => 5,
+        'penality' => 1,
+        'p1' => 0,
+        'p2' => 0,
+        'p3' => 0,
+        'p4' => 0,
+        'p5' => 0,
+        'p6' => 0,
+    ]
+);
+$totaux = [
+    1 => 5,
+    2 => 3.5,
+    3 => 4,
+    4 => 4,
+    5 => 5,
+    6 => 4
+];
